867dc1b79b20f3366633fe58a26f0f4d61269e71
Gn337 <gn337_reart@outlook.com>
2025-07-22 19:46:19 +0800

    初始化项目

---
+++ src/main/java/com/waiit/ailovemasteragent/AiLoveMasterAgentApplication.java
@@ -0,0 +1,13 @@
+package com.waiit.ailovemasteragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLoveMasterAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLoveMasterAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waiit/ailovemasteragent/AiLoveMasterAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waiit.ailovemasteragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLoveMasterAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
